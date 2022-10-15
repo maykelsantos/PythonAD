0b1646f475c058bba18bfcf388a350730f615cfa
Maykel Santos <eumaykelandre@gmail.com>
2022-10-15 17:41:13 -0300

    python

---
+++ Etapa 01/07 fatiamento de strings.py
@@ -0,0 +1,11 @@
+# Fatiamento de Strings
+
+minhaString = 'Aprender Python é top!'
+
+print(type(minhaString))
+print(len(minhaString))
+print(minhaString[0])
+print(minhaString[1])
+print(minhaString[-1])
+print(minhaString[-10:])
+print(minhaString[0:10])
