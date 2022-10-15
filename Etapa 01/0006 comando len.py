740ee552e088933b9e4ec52d2d947efcfcb728d7
Maykel Santos <eumaykelandre@gmail.com>
2022-10-15 17:21:06 -0300

    python

---
+++ Etapa 01/0006 comando len.py
@@ -0,0 +1,14 @@
+# Comando len
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dicionario = {
+    'nome':'maykel',
+    'idade':28
+}
+tupla = (10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+string = 'Fala comigo!'
+
+print(len(lista)) # na 'list' o comando 'len' realiza a contagem dos valores contidos
+print(len(dicionario)) # na 'dict' o comando 'len' realiza a contagam do dos valores e não do 'index'
+print(len(tupla)) # na 'duples' o comando 'len' realiza a contagem dos valores contidos
+print(len(string)) # na 'str' o comando 'len' realiza a contagem dos caracteres
