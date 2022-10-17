33cee905f069b594d38662abfaaef15eaf3f9e60
Maykel Santos <eumaykelandre@gmail.com>
2022-10-17 10:09:49 -0300

    python

--- Etapa 02/06 pacote datetime.py
+++ Etapa 02/06 pacote datetime.py
@@ -1,4 +1,31 @@
 import datetime
 
+# ------------
 hoje = datetime.datetime.today()
 print(hoje)
+print(type(hoje))
+
+# ----------------
+hoje1 = datetime.datetime.today().date()
+print(hoje1)
+
+# ----------------
+data = datetime.datetime.today().date()
+
+ano = data.year
+mes = data.month
+dia = data.day
+
+print(dia, mes, ano)
+
+# -------------
+dataAntiga = datetime.date(1994, 9, 12)
+
+print(hoje1 - dataAntiga) # necessário ser o mesmo formato de data
+
+# Ajuste de formato
+
+print(hoje1.strftime('%d/%m/%y'))
+print(dataAntiga.strftime('%d/%m/%y'))
+
+print(dataAntiga + datetime.timedelta(days=6012))
