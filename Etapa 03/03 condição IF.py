20fa01cc2cbe337114fda0c42defb2c4f52679ec
Maykel Santos <eumaykelandre@gmail.com>
2022-10-19 22:58:31 -0300

    python

---
+++ Etapa 03/03 condição IF.py
@@ -0,0 +1,8 @@
+idade = 16
+
+if idade >= 18:
+    print('Você é maior de idade!')
+elif idade >= 15 and idade <= 17:
+    print('Apesar de não ser maior de idade, seu responsável pode autorizar!')
+else:
+    print('Você NÃO é maior de idade!')
