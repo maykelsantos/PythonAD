8589b5b6e449567a8c40bebe546d4f60df8e876f
Maykel Santos <eumaykelandre@gmail.com>
2022-10-19 23:16:22 -0300

    python

---
+++ Etapa 03/04 laço FOR.py
@@ -0,0 +1,9 @@
+for qualquerCoisa in range(10): # lembrar que a contagem inicia do valor zero '0'
+    print(qualquerCoisa)
+    
+for qualquerCoisa in range(1, 11): # delimitando início e fim
+    print(qualquerCoisa)
+    
+for qualquerCoisa in range(0, 100, 5): # delimitando o espaço de contagem. Nesse caso '5'
+    print(qualquerCoisa)
+
