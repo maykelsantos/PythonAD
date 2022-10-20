c13c63941241c315e2b7dd209987d28d78e070f4
Maykel Santos <eumaykelandre@gmail.com>
2022-10-19 21:51:25 -0300

    python

---
+++ Etapa 02/08 pacote math.py
@@ -0,0 +1,12 @@
+import math
+
+tupla = (10, 5, 20, 30, 40)
+numeroNegativo = -75.54
+
+print(min(tupla)) # 'min' evidencia o menor número
+print(max(tupla)) # 'max' evidencia o maior número
+print(abs(numeroNegativo)) # 'abs' transforma número negativo em positivo
+print(pow(3, 4)) # 'pow' tem a mesma função da potência na matemática (no exemplo: 3 elevado a 4)
+print(math.sqrt(81)) # 'math.sqrt' evidencia a raiz quadrada do valor especificado
+print(math.ceil(1.4)) # 'math.ceil' arredonda o valor especificado para cima
+print(math.floor(1.4)) # 'math.floor' arredonda o valor especificado para baixo
