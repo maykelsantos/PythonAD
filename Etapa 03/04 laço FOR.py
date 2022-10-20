6f79f1ffe5abde101408ca393d1b35fd1f6fda2a
Maykel Santos <eumaykelandre@gmail.com>
2022-10-20 00:00:53 -0300

    python

--- Etapa 03/04 laço FOR.py
+++ Etapa 03/04 laço FOR.py
@@ -1,3 +1,5 @@
+import time
+
 for qualquerCoisa in range(10): # lembrar que a contagem inicia do valor zero '0'
     print(qualquerCoisa)
     
@@ -7,3 +9,19 @@ for qualquerCoisa in range(1, 11): # delimitando início e fim
 for qualquerCoisa in range(0, 100, 5): # delimitando o espaço de contagem. Nesse caso '5'
     print(qualquerCoisa)
 
+paises = ['Brasil', 'Argentina', 'Uruguai', 'Alemanha', 'Cuba', 'Moçambique', 'França']
+
+for percurso in paises:
+    print(percurso)
+    time.sleep(1) # aguarda um segundo para executar o próximo código
+
+for percurso in paises:
+    if percurso == 'Brasil':
+        print(percurso, ', este é o país pentacampeão do mundo!')
+        time.sleep(1) # aguarda um segundo para executar o próximo código
+    elif percurso == 'Alemanha':
+        print(percurso, ', este país ganhou do Brasil de 7 x 1. Placar histórico!')
+        time.sleep(1) # aguarda um segundo para executar o próximo código
+    else:
+        print(percurso)
+        time.sleep(1) # aguarda um segundo para executar o próximo código
