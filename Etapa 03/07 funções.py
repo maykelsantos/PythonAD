80bc483974102e2a7a9b84c4608b075b6c0b6953
Maykel Santos <eumaykelandre@gmail.com>
2022-10-28 22:54:59 -0300

    python

---
+++ Etapa 03/07 funções.py
@@ -0,0 +1,21 @@
+# Exemplo 01 ---------
+
+def python():
+    print('***PYTHON***')
+    
+python()
+
+#Exemplo 02 ---------
+
+def soma(valor1, valor2):
+    soma = valor1 + valor2
+    print(soma)
+    
+soma(2, 5)
+
+for loop in range(0, 10):
+    import random
+    number1 = random.random()
+    number2 = random.random()
+    
+    soma(number1, number2)
