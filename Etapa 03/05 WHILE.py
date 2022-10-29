6e16d598014c1df94308c1cc929dc5cb5b3c631a
Maykel Santos <eumaykelandre@gmail.com>
2022-10-28 22:06:30 -0300

    python

--- Etapa 03/05 WHILE.py
+++ Etapa 03/05 WHILE.py
@@ -1,3 +1,7 @@
+import random
+
+#---------
+
 parar = 0
 
 while parar <= 10:
@@ -13,3 +17,16 @@ while loop <= 10:
         for x in range(10):
             print(x)
     loop = loop + 1
+    
+#----------
+# Jogo de Exemplo
+
+while True:
+    jogador1 = random.randint(0, 10)
+    jogador2 = random.randint(0, 10)
+    print('Jogador 1 tirou o valor ', jogador1)
+    print('Jogador 2 tirou o valor ', jogador2)
+    if jogador1 > jogador2:
+        print('Jogador 1 ganhou o jogo!')
+        break
+    print('\n')
