8d6683abeaa2de338b6af79fbf665688bc5103b4
Maykel Santos <eumaykelandre@gmail.com>
2022-10-28 21:33:48 -0300

    python

---
+++ Etapa 03/05 WHILE.py
@@ -0,0 +1,15 @@
+parar = 0
+
+while parar <= 10:
+    print(parar)
+    parar = parar + 1
+    
+#----------
+
+loop = 0
+while loop <= 10:
+    print(loop)
+    if loop == 5:
+        for x in range(10):
+            print(x)
+    loop = loop + 1
